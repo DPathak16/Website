4c84fb81eaa982c32e229d3d46e73cd07db899ec
Daksh Pathak <dakshds2616@gmail.com>
2025-08-04 02:28:29 -0400

    Refactor EducationComponent to streamline data fetching and improve service integration

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { EducationService } from '../../services/education/education.service';
+import { EducationService } from '../../services/education/education.service';
 import { HttpClient } from '@angular/common/http';
-import { isPlatformBrowser } from '@angular/common';
-import { Inject, PLATFORM_ID } from '@angular/core';
+// import { isPlatformBrowser } from '@angular/common';
+// import { Inject, PLATFORM_ID } from '@angular/core';
 
 
 @Component({
@@ -15,7 +15,7 @@ import { Inject, PLATFORM_ID } from '@angular/core';
 export class EducationComponent implements OnInit {
   educationData: Record<string, Array<any>> = {}; // Updated type to match the JSON structure
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient) {}
+  constructor(private educationService: EducationService) {}
 
   ngOnInit(): void {
     this.fetchEducationData();
@@ -25,18 +25,17 @@ export class EducationComponent implements OnInit {
    * Fetches education data from the service and handles the response.
    */
   fetchEducationData(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.http.get<Record<string, any[]>>('/assets/education/education-data.json').subscribe(
-        data => {
-          this.educationData = data;
-        },
-        error => {
+    this.educationService.getEducationData().subscribe(
+      data => {
+        this.educationData = data;
+      },
+      error => {
           console.error('Error fetching education data:', error);
           this.educationData = {}; // Fallback to an empty object
         }
       );
     }
-  }
+  
 
   /**
    * Retrieves the list of countries from the education data.
